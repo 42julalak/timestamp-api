e90242bb8dd43e770617436fe29abf1bfc689ea5
unknown <herohrx1125@gmail.com>
2020-10-21 01:05:49 +0700

    bgfx: change sms method and unaccess who was deleted

--- src/function/cron.js
+++ src/function/cron.js
@@ -1,11 +1,15 @@
 const CronJob = require("cron").CronJob;
 const Sms = require("../function/sms")
+const model = require("../models/db")
+const statusEnum = require("../enum/status.enum")
 
 require("dotenv").config();
 
-const lateAlert = new CronJob("30 13 1 * * *", function () {
+const lateAlert = new CronJob("20 3 1 * * *", async function () {
   console.log('start sending late alert')
-  const tels = ['66650616536', '66877992347']
+  const users = await model.user.find({ status: statusEnum.ACTIVE });
+  const tels = users.map((member) => member.tel)
+
   for (const tel of tels) {
     Sms.send({
       to: tel,
